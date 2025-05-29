db03079bc44f54491cf05e9d2d7cad4ba239d6a6
MIN <kimmin6303@gmail.com>
2025-05-29 12:17:43 +0900

    최초실행시에 이름 입력

--- Sample/src/full/java/com/live2d/demo/full/calendar/NameInputActivity.java
+++ Sample/src/full/java/com/live2d/demo/full/calendar/NameInputActivity.java
@@ -2,6 +2,7 @@ package com.live2d.demo.full.calendar;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,18 +18,25 @@ public class NameInputActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // userName이 이미 저장되어 있으면 바로 MainActivity로 이동
+        if (!isFirstLaunch()) {
+            startActivity(new Intent(NameInputActivity.this, MainActivity.class));
+            finish();
+            return;
+        }
+
         setContentView(R.layout.activity_name_input);
+        getSupportActionBar().hide();
 
         EditText etName = findViewById(R.id.etName);
         Button btnSubmit = findViewById(R.id.btnSubmit);
-        getSupportActionBar().hide();
 
         btnSubmit.setOnClickListener(v -> {
             String name = etName.getText().toString().trim();
             if (!name.isEmpty()) {
                 getSharedPreferences("MyAppPrefs", Context.MODE_PRIVATE)
                         .edit()
-                        .putBoolean("isFirstLaunch", false)
                         .putString("userName", name)
                         .apply();
 
@@ -39,5 +47,10 @@ public class NameInputActivity extends BaseActivity {
                 showCustomToast("이름을 입력하세요.");
             }
         });
+    }
+    private boolean isFirstLaunch() {
+        SharedPreferences prefs = getSharedPreferences("MyAppPrefs", MODE_PRIVATE);
+        String userName = prefs.getString("userName", null);
+        return (userName == null || userName.isEmpty());
     }
 }
